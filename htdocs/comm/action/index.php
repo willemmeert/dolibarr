02a04c9be22e8614d6912a116f0cf3ed117600d2
Willem Meert <willemmeert@users.noreply.github.com>
2021-11-11 21:05:41 +0100

    hook / note / language
    Updates from 13.0 migrated:
      Add hook for reporting in agenda;
      Show note in agenda;
      Update language files;

--- htdocs/comm/action/index.php
+++ htdocs/comm/action/index.php
@@ -36,6 +36,14 @@ require_once DOL_DOCUMENT_ROOT.'/contact/class/contact.class.php';
 require_once DOL_DOCUMENT_ROOT.'/comm/action/class/actioncomm.class.php';
 require_once DOL_DOCUMENT_ROOT.'/core/lib/date.lib.php';
 require_once DOL_DOCUMENT_ROOT.'/core/lib/agenda.lib.php';
+
+// Eclipse definitions
+/** @var Translate $langs **/
+/** @var User $user **/
+/** @var DoliDB $db **/
+/** @var HookManager $hookmanager **/
+/** @var Conf $conf **/
+
 if (!empty($conf->projet->enabled)) {
 	require_once DOL_DOCUMENT_ROOT.'/core/class/html.formprojet.class.php';
 }
@@ -627,7 +635,7 @@ $sql = 'SELECT ';
 if ($usergroup > 0) {
 	$sql .= " DISTINCT";
 }
-$sql .= ' a.id, a.label,';
+$sql .= ' a.id, a.label, a.note,';
 $sql .= ' a.datep,';
 $sql .= ' a.datep2,';
 $sql .= ' a.percent,';
@@ -793,6 +801,7 @@ if ($resql) {
 
 		$event->libelle = $obj->label; // deprecated
 		$event->label = $obj->label;
+		$event->note_private = $obj->note;
 		$event->percentage = $obj->percent;
 
 		$event->authorid = $obj->fk_user_author; // user id of creator
